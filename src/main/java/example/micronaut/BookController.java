3b1da8c20f2f9bca14e192f69aa4edd13ee0b1d8
Stealth-PC <anubis.delta@yandex.ru>
2023-06-06 09:06:45 +0300

    feat: Global @Error

--- src/main/java/example/micronaut/BookController.java
+++ src/main/java/example/micronaut/BookController.java
@@ -1,15 +1,18 @@
 package example.micronaut;
 
-import io.micronaut.context.MessageSource;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
+import io.micronaut.http.annotation.Error;
 import io.micronaut.views.View;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller("/books")
 @RequiredArgsConstructor
@@ -35,4 +38,20 @@ public class BookController {
                 model.put("pages", "");
                 return model;
     }
+
+    @View("bookscreate")
+    @Error(exception = ConstraintViolationException.class)
+    public Map<String, Object> onSavedFailed(HttpRequest request, ConstraintViolationException ex) {
+        final Map<String, Object> model = createModelWithBlankValues();
+        model.put("errors", messageSource.violationsMessages(ex.getConstraintViolations()));
+        Optional<CommandBookSave> cmd = request.getBody(CommandBookSave.class);
+        cmd.ifPresent(bookSave -> populateModel(model, bookSave));
+        return model;
+    }
+
+    private void populateModel(Map<String, Object> model, CommandBookSave bookSave) {
+        model.put("title", bookSave.getTitle());
+        model.put("pages", bookSave.getPages());
+    }
+
 }
